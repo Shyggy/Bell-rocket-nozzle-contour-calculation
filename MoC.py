2ba5149640e9047704db0d94315ffac8ff26114c
Jędrzej <mr.myszer@gmail.com>
2026-01-03 23:23:19 +0100

    moc_first_steps

--- MoC.py
+++ MoC.py
@@ -46,6 +46,32 @@ def Hall_IPM_fun(nu,gamma):
     M=(1+d1*y+d2*y**2+d3*y**3)/(1+e1*y+e2*y**2)
     return M
 
+def node_value_calculator(n_char,theta_start,gamma,Mach_exit):
+    #initial values calculation
+    n_nodes=node_volume_finder(n_char) #number of nodes in the characteristic mesh
+    theta_end=PM_fun(Mach_exit,gamma)/2 #throat exit flow halfangle in radians
+    theta_step=(theta_end-theta_start)/(n_char-1) #step size for theta values
+    #memory allocation for nodes
+    K_plus_values=np.zeros(n_nodes)  #Right characteristic values at each node
+    K_minus_values=np.zeros(n_nodes) #Left characteristic values at each node
+    theta_values=np.zeros(n_nodes) #flow angle values at each node
+    nu_values=np.zeros(n_nodes) #Prandtl-Meyer function values at each node
+    mu_values=np.zeros(n_nodes) #Mach angle values at each node
+    M_values=np.zeros(n_nodes) #Mach number values at each node
+    #initial values on the characteristic kibes
+    theta_values[0:n_char]=nu_values[0:n_char]=np.arange(theta_start,theta_end+theta_step/2,theta_step) #initial theta and nu values along the characteristic lines
+    K_plus_values[0:n_char]=theta_values[0:n_char]-nu_values[0:n_char] #initial K+ values along the characteristic lines
+    K_minus_values[0:n_char]=theta_values[0:n_char]+nu_values[0:n_char] #initial K- values along the characteristic lines
+    M_values[0:n_char]=Hall_IPM_fun(theta_values[0:n_char],gamma) #initial Mach number values along the characteristic lines
+    mu_values[0:n_char]=Mach_angle_calculator(M_values[0:n_char]) #initial Mach angle values along the characteristic lines
+
+
+
+    output=np.column_stack((np.rad2deg(K_minus_values),np.rad2deg(K_plus_values),np.rad2deg(theta_values),np.rad2deg(nu_values),M_values,np.rad2deg(mu_values)))
+    return output
+    
+
+
 
 
 n_char=int(7) #number of characteristic lines to be drawn
@@ -53,9 +79,13 @@ n_nodes=node_volume_finder(n_char) #number of nodes in the characteristic mesh
 x_0,y_0=0,1 #location of the first node
 M_e=2.4 #exit Mach number
 gamma=1.4 #specific heat ratio for exhaust gases
-theta_s=0.00655 # first characteristic line flow angle in radians
+theta_s=np.deg2rad(0.375) # first characteristic line flow angle in radians
 theta_e=PM_fun(M_e,gamma)/2 #exit flow angle in radians4
 init_theta_vect=np.linspace(theta_s,theta_e,n_char) #initial theta values for the characteristic lines
+
+test=node_value_calculator(n_char,theta_s,gamma,M_e)
+
+
 
 theta1=theta_e
 theta2=theta_s
@@ -73,6 +103,7 @@ print(x_t,x_t2,x_t3)
 print(np.rad2deg(theta_e))
 print(np.rad2deg(init_theta_vect))
 print(Hall_IPM_fun(theta_e*2,gamma))
+print(test)
 plt.figure()
 plt.plot([x_0,x_t],[y_0,y_t])
 plt.plot([x_0,x_t2],[y_0,y_t2])
